aab39d15c2810cbdea904c6f997f2db02e51b3a8
Michal Altair Valášek <github@rider.cz>
2014-08-29 15:43:18 +0200

    Updated library metadata.

--- Altairis.Mvc.Localization/Properties/AssemblyInfo.cs
+++ Altairis.Mvc.Localization/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Altairis.Mvc.Localization")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Altairis.Mvc.Localization")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df79858f-21a9-4a05-bee2-6dc0f618398d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyTitle("Altairis MVC Localization Toolkit")]
+[assembly: AssemblyDescription("Localization toolkit for ASP.NET MVC")]
+[assembly: AssemblyCompany("Altairis, s. r. o.")]
+[assembly: AssemblyProduct("Altairis MVC Localization Toolkit")]
+[assembly: AssemblyCopyright("Copyright © Altairis, 2014")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
